31c9c435759deaf9afc384ebcca3fdcbb3f46833
Michael McConville <momcconville@gmail.com>
2014-11-29 17:11:50 -0500

    comment tweak

--- gofmt.sh
+++ gofmt.sh
@@ -1,4 +1,4 @@
-# A Bash two-liner to defy gc's attempt to force me to use tabs by removing the -tabs option from gofmt.
+# A shell two-liner to defy gc's attempt to force me to use tabs by removing the -tabs option from gofmt.
 
 gofmt -s=true -w=true repeatgenome/*.go
 perl -pi -e 's/\t/    /g' repeatgenome/*.go
