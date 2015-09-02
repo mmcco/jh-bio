ca08e02d41ebea5600f51aa8244258a69c32d6c6
Michael McConville <mmcconville ~a~ mykolab ~d~ com>
2015-09-02 14:01:07 -0400

    removing gofmt.sh, complying with fascist tab mandate

--- gofmt.sh
+++
@@ -1,4 +0,0 @@
-# A shell two-liner to defy gc's attempt to force me to use tabs by removing the -tabs option from gofmt.
-
-gofmt -s=true -w=true *.go repeatgenome/*.go
-perl -pi -e 's/\t/    /g' *.go repeatgenome/*.go
